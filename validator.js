13f784145b5c949746d44d6fe70a4c8d9e4297fb
Richard <fousekrichard@gmail.com>
2026-06-12 01:24:14 +0200

    initial commit

---
+++ validator.js
@@ -0,0 +1,14 @@
+// exports.functionName = (parameters) => {}
+
+function isInvalidEmail(userObj) {
+    return !userObj.email.includes("@")
+}
+
+function isEmptyPayload(userObj) {
+    return Object.keys(userObj).length === 0
+}
+
+module.exports = {
+    isInvalidEmail,
+    isEmptyPayload
+}
